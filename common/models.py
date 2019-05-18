f93563831d2ecec57cd7d178bf46948cee14fa73
Your Name <abhishek.bharadwaj@healthify.co>
2019-05-18 21:19:35 +0530

    Added common module

---
+++ common/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
