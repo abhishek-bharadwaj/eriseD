aba2fd639fc43df66de73e169d22309ccfc7e28e
Your Name <abhishek.bharadwaj@healthify.co>
2019-05-18 21:19:14 +0530

    Added models

--- mock/models.py
+++ mock/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class Store(BaseModel):
+    username = models.ForeignKey(User, related_name='mocks', on_delete=models.CASCADE, null=True)
+    url = models.CharField(max_length=100, primary_key=True)
+    data = JSONField()
+    response_delay = models.IntegerField()
