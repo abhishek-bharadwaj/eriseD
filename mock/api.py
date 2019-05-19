6b4259e291e68a1d1e1851bc24f979fc8f0428b7
Your Name <abhishek.bharadwaj@healthify.co>
2019-05-19 22:24:30 +0530

    Added form

--- mock/api.py
+++ mock/api.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.utils import timezone
 
 from tastypie.resources import ModelResource
 from tastypie.utils.urls import trailing_slash
@@ -6,6 +7,8 @@ from tastypie.utils.urls import trailing_slash
 from mock.models import Store
 from mock import public
 from mock import views
+
+from . import forms
 
 
 class MockResource(ModelResource):
@@ -19,9 +22,26 @@ class MockResource(ModelResource):
             url(r'^(?P<resource_name>%s)/get_data%s$' %
                 (self.Meta.resource_name, trailing_slash()),
                 self.wrap_view('get_data'), name='get_json_data_for_url'),
+            url(r'^(?P<resource_name>%s)/create%s$' %
+                (self.Meta.resource_name, trailing_slash()),
+                self.wrap_view('create_mock'), name='create_and_store_mock'),
         ]
 
     def get_data(self, request, **kwargs):
         self.method_check(request, ['get'])
         searched_url = request.GET['url'].strip()
         return views.render_data(request, public.get_json_data_for_url(searched_url) or {})
+
+    def create_mock(self, request, **kwargs):
+        return views.post_new(request)
+        # if request.method == "POST":
+        #     form = forms.AddressForm(request.POST)
+        #     if form.is_valid():
+        #         post = form.save(commit=False)
+        #         post.author = request.user
+        #         post.published_date = timezone.now()
+        #         post.save()
+        #         return self.create_response(request, {'message': 'Mock created successfully..'})
+        # else:
+        #     form = forms.AddressForm()
+        # return render(request, 'blog/post_edit.html', {'form': form})
--- mock/views.py
+++ mock/views.py
@@ -2,12 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from mock import public
+from . import forms
 
 
-def render_data(request, url):
+def render_data(request, return_data):
     return_response_template = loader.get_template('return_response.html')
     context = {
 
     }
     return HttpResponse(return_response_template.render(context, request))
+
+
+def post_new(request):
+    form = forms.AddressForm()
+    return render(request, 'store_mocks.html', {'form': form})
