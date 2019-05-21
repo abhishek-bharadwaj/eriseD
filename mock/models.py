05e4bd628b11dfcd3ccb4161cb7fd2e564acf601
Your Name <abhishek.bharadwaj@healthify.co>
2019-05-21 18:45:09 +0530

    Fixed review comment

--- mock/models.py
+++ mock/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-from . import private
+from mock import private
 from common.models import BaseModel
 
 
